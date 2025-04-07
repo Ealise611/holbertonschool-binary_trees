9d57765bcf7ea3ad621e86f2f4038c0e59efc4f8
Ealise <10503@holbertonstudents.com>
2025-04-07 07:59:17 +0000

    preorder travesal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,11 +5,14 @@
  * binary_tree_preorder - Traverses a binary tree using pre-order traversal
  *
  * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to the function to call for each node. The node's value is passed as an argument.
+ * @func: Pointer to the function to call for each node.
+ * The node's value is passed as an argument.
  *
- * Description: This function traverses a binary tree in pre-order, meaning it processes the
- * current node first, then recursively traverses the left subtree, followed by the right subtree.
- * The value of each node is passed to the provided function `func` for processing.
+ * Description: traverses a binary tree in pre-order, meaning it processes the
+ * current node first, then recursively traverses the left subtree,
+ * followed by the right subtree.
+ * The value of each node is passed to
+ * the provided function `func` for processing.
  * If either the tree or the function is NULL, the function does nothing.
  *
  * Return: None
